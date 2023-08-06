ca511c0ec2a1e9459a9827b0d6af25cfdf370e86
sahapranta <pranta1204@gmail.com>
2023-08-06 12:45:22 +0600

    title changed for CI test

--- resources/views/layouts/simple.blade.php
+++ resources/views/layouts/simple.blade.php
@@ -5,9 +5,9 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
-  <title>OneUI - Bootstrap 5 Admin Template &amp; UI Framework</title>
+  <title>{{ AppSettings::get('site_name', 'Laravel') }} | {{ AppSettings::get('site_title', 'Welcome to our Digital Store!') }}</title>
 
-  <meta name="description" content="OneUI - Bootstrap 5 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+  <meta name="description" content="{{ AppSettings::get('site_name', 'Laravel') }} {{ AppSettings::get('site_title', 'Welcome to our Digital Store!') }}">
   <meta name="author" content="pixelcave">
   <meta name="robots" content="noindex, nofollow">
 
